c9e9aca1fcaf85061dd41cd85aaadcbd560d62c2
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-05-03 13:42:28 +0530

    added the heatmap

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stats from '../components/Stats'
+import FocusHeatmap from '../components/FocusHeatmap'
 
 const StatsPage = () => {
   return (
-    <div>
+    <div className='stats-page'>
       <Stats/>
+      <FocusHeatmap/>
     </div>
   )
 }
